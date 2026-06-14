56d732486169e2edee0375c2f01dde2cc43fe11a
Birgit Vosar <birgit.vosar@gmail.com>
2026-06-14 14:09:23 +0300

    fetched form with specific id data

--- app/(dashboard)/forms/[formId]/builder/page.tsx
+++ app/(dashboard)/forms/[formId]/builder/page.tsx
@@ -1,38 +1,29 @@
 'use client'
 import Sidebar from '@/components/Sidebar'
-import Card from '@/components/Card'
 import { useEffect, useState } from 'react'
-import DeleteCard from '@/components/DeleteCard'
-import { useRouter } from 'next/navigation'
+import { useParams } from 'next/navigation'
 
 type Form = {
-    id: number,
-    title: string,
-    is_published: boolean,
-    responses_amount: number,
-    last_response: string
+    title:string
 }
 
 export default function DashboardPage() {
-    const router = useRouter()
+    const { formId } = useParams()
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState<string>('')
-    const [deleteCardOpen, setDeleteCardOpen] = useState(false)
-    const [formTitle, setFormTitle] = useState<string>('')
-    const [formId, setFormId] = useState<number>(0)
-    const [forms, setForms] = useState<Form[]>([])
+    const [form, setForm] = useState<Form | null>({})
 
     useEffect(() => {
         setLoading(true)
         const fetchForms = async () => {
             try {
-                const res = await fetch('/api/forms');
+                const res = await fetch(`/api/forms/${formId}`);
                 if (!res.ok) {
                     setError('Something went wrong with fetching the forms.')
                     return
                 }
                 const data = await res.json()
-                setForms(data)
+                setForm(data)
             } catch (err) {
                 setError('Something went wrong with fetching the forms.')
             } finally {
@@ -41,58 +32,6 @@ export default function DashboardPage() {
         }
         fetchForms()
     }, [])
-
-    async function createForm() {
-        const res = await fetch('/api/forms', {
-            method: 'POST'
-        })
-
-        if (!res.ok) {
-            setError('Failed to create new form, please try again.')
-        }
-        const formId = res.json()
-        console.log('this is client side res:', res)
-        router.push(`/forms/${formId}/builder`)
-    }
-
-    const handleDelete = ({ id, title }: { id: number, title: string }) => {
-        setDeleteCardOpen(true)
-        setFormTitle(title)
-        setFormId(id)
-        return
-    }
-
-    async function handleDeleteConfirm() {
-        setLoading(true)
-        setError('')
-        console.log('Sending info to database, that form with this id:', formId, 'has to be deleted')
-        const result = await fetch(`/api/forms/${formId}`, {
-            method: 'DELETE',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formId)
-        })
-        if (!result.ok) {
-            setError('Something went wrong with deleting the form, please try again.')
-            setLoading(false)
-            return
-        }
-        setForms(prev => prev.filter(form => form.id !== formId))
-        setFormTitle('')
-        setFormId(0)
-        setDeleteCardOpen(false)
-        setLoading(false)
-        return
-    }
-
-    const handleDeleteCancel = () => {
-        setDeleteCardOpen(false)
-        setFormTitle('')
-        setFormId(0)
-        setError('')
-        return
-    }
 
     return (
         <div className='flex flex-row h-screen overflow-hidden'>
@@ -107,11 +46,11 @@ export default function DashboardPage() {
                                 <div className='bg-white px-2 pb-2 border-b-1 pt-2 border-gray-300 flex justify-between items-center'>
                                     <div className='flex flex-row justify-between px-4 py-1'>
                                         <div className='flex flex-col gap-2'>
-                                            <p className='font-mono text-md font-semibold'>Untitled form</p>
+                                            <p className='font-mono text-md font-semibold'>{form.title}</p>
                                         </div>
                                     </div>
                                     <div>
-                                        <button onClick={createForm} className='cursor-pointer hover:bg-[#004E89] py-2 px-4 mr-4 border rounded-lg border-[#1A659E] bg-[#1A659E] text-white font-semibold font-sans tracking-wide text-sm'>
+                                        <button className='cursor-pointer hover:bg-[#004E89] py-2 px-4 mr-4 border rounded-lg border-[#1A659E] bg-[#1A659E] text-white font-semibold font-sans tracking-wide text-sm'>
                                             + Create new form
                                         </button>
                                     </div>
@@ -127,7 +66,7 @@ export default function DashboardPage() {
                                     <div className='bg-white flex-1 self-start flex flex-row mx-6 px-4 py-6 my-6 border rounded-xl border-gray-300 text-sm'>
                                         <div className='flex-1 flex flex-col gap-4'>
                                             <div className='flex gap-2 '>
-                                                <p className='font-semibold font-sans'>Untitled form</p>
+                                                <p className='font-semibold font-sans'>{form.title}</p>
                                             </div>
                                             <textarea placeholder='Add a description...'>
                                             </textarea>
--- app/api/forms/[formId]/route.ts
+++ app/api/forms/[formId]/route.ts
@@ -12,7 +12,7 @@ export async function DELETE(req: NextRequest) {
     const formId = await req.json()
     try {
         await pool.query(
-            'DELETE FROM forms WHERE id = $1', [formId]
+            'DELETE FROM forms WHERE id = $1 AND user_id = $2', [formId, userId]
         )
 
         return NextResponse.json({ response: 'Form successfully deleted' }, { status: 200 })
@@ -20,4 +20,27 @@ export async function DELETE(req: NextRequest) {
         console.error('Failed to delete form in api:', err)
         return NextResponse.json({ error: 'Failed to delete form in api' }, { status: 500 })
     }
+}
+
+export async function GET(req: NextRequest, context: { params: { formId: string }}) {
+    const session = await getIronSession<SessionData>(req, new NextResponse(), sessionOptions)
+    const userId = session.userId
+    if (!userId) {
+        return Response.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    try {
+        const { formId } = await context.params
+        const res = await pool.query(
+            'SELECT * FROM forms WHERE id = $1 AND user_id = $2', [formId, userId]
+        )
+        if(res.rows.length === 0) {
+            return NextResponse.json({ response: 'Form not found' }, { status: 404})
+        }
+        return NextResponse.json(res.rows[0])
+    } catch(err) {
+        console.log('failed to fetch info for the specific form')
+        return NextResponse.json({ error: 'Failed to fetch info for the specific form' }, { status: 500 })
+    }
+    
 }
