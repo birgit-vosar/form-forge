e0fe51ed8a4dfc8b8bb8734f45aabe89179fcf03
Birgit Vosar <birgit.vosar@gmail.com>
2026-08-02 11:57:08 +0300

    made checkbox and dropdown responsive

--- components/builder/fields/DropdownFieldInput.tsx
+++ components/builder/fields/DropdownFieldInput.tsx
@@ -4,13 +4,12 @@ export default function DropdownFieldInput({ field }: FieldInputProps) {
     return (
         <div>
             <select className="py-1 w-full px-2 border rounded-sm border-[#8ed0b8] bg-[#dbf1e9] text-sm">
-                <option value="">{field.placeholder ?? '-- Please choose an option --'}</option>
-                <option value="dog">Dog</option>
-                <option value="cat">Cat</option>
-                <option value="hamster">Hamster</option>
-                <option value="parrot">Parrot</option>
-                <option value="spider">Spider</option>
-                <option value="goldfish">Goldfish</option>
+                {field.options?.map((option) => {
+                    const optionId = `option-${option.id}`
+                    return (
+                        <option id={optionId} value={option.value ?? ''}>{option.label ?? '-- Please choose an option --'}</option>
+                    )
+                })}
             </select>
         </div>
     )
