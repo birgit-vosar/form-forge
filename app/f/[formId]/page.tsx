f4ec06d92da98fe5efa6c861223745a604ae2cee
Birgit Vosar <birgit.vosar@gmail.com>
2026-09-24 21:09:57 +0300

    added title and changed responsive design

--- app/f/[formId]/page.tsx
+++ app/f/[formId]/page.tsx
@@ -54,7 +54,7 @@ export default function FormsPage() {
                             <div className='flex-1 flex flex-col overflow-hidden'>
                                 <div className='bg-white px-2 pb-2 border-b-1 pt-2 border-gray-300 flex justify-between items-center'>
                                     <div className='flex flex-row justify-between px-4 py-1'>
-                                        <div className='flex flex-col gap-2'>
+                                        <div className='flex flex-row gap-3 items-center'>
                                             <p className='font-mono text-md font-semibold'>{form?.title}</p>
                                         </div>
                                     </div>
@@ -64,30 +64,26 @@ export default function FormsPage() {
                                         <div className={error ? 'block flex bg-red-500/20 flex-1 max-h-10 border-b-2 border-red-300 py-2 px-4' : 'hidden'}>
                                             <p className='text-red-400 font-sans text-sm'>{error}</p>
                                         </div>
-                                        <div className='bg-[#B7E0D8] flex-1 flex flex-row mx-6 lg:mx-10 px-4 py-6 my-6 border rounded-xl border-[#8ed0b8] text-sm shadow-lg'>
+                                        <div className='bg-[#B7E0D8] flex-1 flex flex-row mx-6 md:mx-40 lg:mx-70 xl:mx-100 2xl:mx-120 px-4 py-6 my-6 border rounded-xl border-[#8ed0b8] text-sm shadow-lg'>
+
                                             <div className='flex-1 flex flex-col gap-4 '>
+                                                <div className='px-2 mb-2 pt-2 flex justify-between items-center'>
+                                                    <div className='pb-6 flex flex-row justify-between py-1 border-b-2  border-[#6ca692] w-full'>
+                                                            <p className='font-mono text-xl font-semibold'>{form?.title}</p>
+                                                    </div>
+                                                </div>
                                                 {
                                                     form?.fields.map((field) => (
-                                                       <div  className={`flex flex-col gap-1 mb-4 py-4 px-2 border-2 rounded-md bg-[#9ed4c9] border-[#6ca692] active:scale-99 active:shadow-sm active:shadow-[#6ca692]/50 `} 
-                                                                   key={field.id}>
-                                                                   <div className="flex justify-between">
-                                                                       <div className="flex gap-1 items-center">
-                                                                           <span className="cursor-grab active:cursor-grabbing mr-1 text-gray-800/50 hover:text-teal-800">
-                                                                               <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="size-4">
-                                                                                   <circle cx="8" cy="6" r="1.5" />
-                                                                                   <circle cx="8" cy="12" r="1.5" />
-                                                                                   <circle cx="8" cy="18" r="1.5" />
-                                                                                   <circle cx="16" cy="6" r="1.5" />
-                                                                                   <circle cx="16" cy="12" r="1.5" />
-                                                                                   <circle cx="16" cy="18" r="1.5" />
-                                                                               </svg>
-                                                                           </span>
-                                                                           <label className='text-black font-mono text-md font-semibold mb-1'>{field.label}</label>
-                                                                           {field.required === true ? (<p className="font-mono text-md font-semibold">*</p>) : ('')}
-                                                                       </div>
-                                                                   </div>
-                                                                   <FieldRenderer field={field} />
-                                                               </div>
+                                                        <div className={`flex flex-col gap-1 px-2 active:scale-99 active:shadow-sm active:shadow-[#6ca692]/50 `}
+                                                            key={field.id}>
+                                                            <div className="flex justify-between">
+                                                                <div className="flex gap-1 items-center">
+                                                                    <label className='text-black font-mono text-md font-semibold mb-1'>{field.label}</label>
+                                                                    {field.required === true ? (<p className="font-mono text-md font-semibold">*</p>) : ('')}
+                                                                </div>
+                                                            </div>
+                                                            <FieldRenderer field={field} />
+                                                        </div>
                                                     ))
                                                 }
 
